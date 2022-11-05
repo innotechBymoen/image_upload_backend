d08020c040578df176fef71e6b7d91b9b8aeef7a
bymoen <alexb@innotechcollege.com>
2022-11-05 07:54:37 -0600

    Getting ready for students

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, make_response, send_from_directory
+import json
+from dbhelpers import run_statement
+from apihelpers import check_endpoint_info, save_file
+import dbcreds
+
+
+app = Flask(__name__)
+
+@app.post('/api/image')
+def post_image():
+    # Checking to see if the request.form contains the needed description
+    # You might have different fields required for your upload
+    is_valid = check_endpoint_info(request.form, ['description'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+
+    # Use request.files to make sure the uploaded_image is there
+    # Again you can call it whatever you would like
+    is_valid = check_endpoint_info(request.files, ['uploaded_image'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+
+    # Save the image using the helper found in apihelpers
+    filename = save_file(request.files['uploaded_image'])
+    # If the filename is None something has gone wrong
+    if(filename == None):
+        return make_response(json.dumps("Sorry, something has gone wrong"), 500)
+
+    # Add row to DB like normal containing the information about the uploaded image
+    results = run_statement('CALL image_create(?,?)', [filename, request.form['description']])
+
+    if(type(results) == list):
+        return make_response(json.dumps('Success'), 200)
+    else:
+        return make_response(json.dumps(results), 500)
+
+@app.get('/api/image')
+def get_image():
+    # Make sure the image_id is sent
+    is_valid = check_endpoint_info(request.args, ['image_id'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+
+    # Get the image information from the DB
+    results = run_statement('CALL get_image(?)', [request.args.get('image_id')])
+    # Make sure something came back from the DB that wasn't an error
+    if(type(results) != list):
+        return make_response(json.dumps(results), 500)
+    elif(len(results) == 0):
+        return make_response(json.dumps("Invalid image id"), 400)
+
+    # Use the built in flask function send_from_directory
+    # First into the images folder, and then use my results from my DB interaction to get the name of the file
+    return send_from_directory('images', results[0]['file_name'])
+
+
+if(dbcreds.production_mode == True):
+    print("Running in Production Mode")
+    import bjoern # type: ignore
+    bjoern.run(app, "0.0.0.0", 5000)
+else:
+    from flask_cors import CORS
+    CORS(app)
+    print("Running in Testing Mode")
+    app.run(debug=True)
---
+++ db.sql
@@ -0,0 +1,88 @@
+-- MariaDB dump 10.19  Distrib 10.8.3-MariaDB, for osx10.15 (x86_64)
+--
+-- Host: localhost    Database: assignment_testing
+-- ------------------------------------------------------
+-- Server version	10.8.3-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `image`
+--
+
+DROP TABLE IF EXISTS `image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `image` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(100) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'assignment_testing'
+--
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `get_image` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_image`(image_id_input INT UNSIGNED)
+BEGIN
+	SELECT CONVERT(file_name using utf8) as file_name, created_at FROM image WHERE id = image_id_input;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `image_create` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `image_create`(file_name_input VARCHAR(100), description_input VARCHAR(255))
+    MODIFIES SQL DATA
+BEGIN
+	INSERT INTO image(file_name, description) VALUES (file_name_input, description_input);
+	SELECT LAST_INSERT_ID();
+	COMMIT;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-04 14:18:27
